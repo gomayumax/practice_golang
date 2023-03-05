ad821d69ea57ecfa87f4836fd88ca1b293a956c5
chimu <ishikawa@studiounbuilt.com>
2023-03-06 08:40:09 +0900

    DB接続

--- gin-api/project/main.go
+++ gin-api/project/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"gin-api/project/db"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,8 +10,10 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello, World")
 	})
+	db.Init()
 	err := r.Run()
 	if err != nil {
 		return
 	}
+	db.Close()
 }
